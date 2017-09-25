acaba5cc3d640148388b167d4b95a841dc608e41
Christian Fobel <christian@fobel.net>
2017-09-25 14:36:17 -0400

    Fix build recipe

--- setup.py
+++ setup.py
@@ -5,14 +5,9 @@ from __future__ import print_function
 
 from glob import glob
 from os.path import basename
-from os.path import dirname
-from os.path import join
 from os.path import splitext
 from setuptools import find_packages
 from setuptools import setup
-import io
-import re
-import sys
 
 import versioneer
 
@@ -28,7 +23,7 @@ setup(name='pyusb-helpers',
       license='BSD',
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      install_requires=['openpyxl'],
+      install_requires=['conda-helpers', 'pyusb'],
       py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
       # Install data listed in `MANIFEST.in`
       include_package_data=True)
